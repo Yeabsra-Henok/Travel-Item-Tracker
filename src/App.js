9c2f3fe070015381005290230784be757ed70aef
Yeabsra Henok <75983453+Yeabsra-Henok@users.noreply.github.com>
2024-05-05 16:44:42 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+//this thunder works like magic. I wonder who sent it. perhaps the middle ravens? //
+
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },

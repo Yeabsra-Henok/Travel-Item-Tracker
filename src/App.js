a1beb2d09ae9f9dc77bef99202c622d10ed98cf3
Yeabsra Henok <yeabsrahenok0909@gmail.com>
2024-05-05 12:28:31 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Log />
+      <Form />
+      <Packinglist />
+      <Stats />
     </div>
   );
 }
 
-export default App;
+function Log() {
+  return <h1>🌴Far Away 💼 </h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your trip</h3>
+    </div>
+  );
+}
+
+function Packinglist() {
+  return <ul className="list">LIST</ul>;
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em> You Have X items on your list and you already packed X (X%) </em>
+    </footer>
+  );
+}

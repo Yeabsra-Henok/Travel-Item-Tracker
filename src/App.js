4ad07aa26b1889405f0ad15dc3d246c337e1edae
Yeabsra Henok <yeabsrahenok0909@gmail.com>
2024-05-06 13:10:56 +0300

    commit04

--- src/App.js
+++ src/App.js
@@ -1,6 +1,3 @@
-//this thunder works like magic. I wonder who sent it. perhaps the middle ravens? //
-
-
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
@@ -54,7 +51,6 @@ function Packinglist() {
     </div>
   );
 }
-//test
 
 function Stats() {
   return (

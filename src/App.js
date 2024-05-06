9392c32b11cbb65c26f8236c61b4cd0f7c3415e3
Yeabsra Henok <yeabsrahenok0909@gmail.com>
2024-05-06 15:34:05 +0300

    I learned controlled state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Charger", quantity: 1, packed: true },
 ];
 
 export default function App() {
@@ -10,6 +13,7 @@ export default function App() {
       <Form />
       <Packinglist />
       <Stats />
+      <Test3 />
     </div>
   );
 }
@@ -19,21 +23,41 @@ function Log() {
 }
 
 function Form() {
-  return (
-    <div className="add-form">
-      <h3>What do you need for your trip</h3>
-    </div>
-  );
-}
-function Item({ item }) {
-  return (
-    <li>
-      <span>
-        {item.quantity} {item.description}
-      </span>
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(5);
 
-      <button>❌</button>
-    </li>
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your trip</h3>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {/*
+        this does the same job..
+        <option value={1}>1</option>
+        <option value={2}>3</option>
+        <option value={3}>1</option> 
+        as this one 👇
+        */}
+
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button> Add </button>
+    </form>
   );
 }
 
@@ -44,11 +68,23 @@ function Packinglist() {
         {initialItems.map(
           //the call back function...
           (item) => (
-            <Item item={item} />
+            <Item item={item} key={item.id} />
           )
         )}
       </ul>
     </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+
+      <button>❌</button>
+    </li>
   );
 }
 
@@ -58,4 +94,8 @@ function Stats() {
       <em> You Have X items on your list and you already packed X (X%) </em>
     </footer>
   );
+}
+
+function Test3() {
+  return null;
 }

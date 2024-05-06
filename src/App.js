ce8f29fb50b94bd34eb475618310ca836dd1c9a3
Yeabsra Henok <75983453+Yeabsra-Henok@users.noreply.github.com>
2024-05-06 16:50:55 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-//this is a test
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },

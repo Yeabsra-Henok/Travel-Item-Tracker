3d58bb0b543a9fd4d937fa653ece08b158ba83fc
Yeabsra Henok <yeabsrahenok0909@gmail.com>
2024-05-11 15:28:53 +0300

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 2, description: "charger", quantity: 1, packed: true },
 ];
 
 export default function App() {
@@ -27,7 +28,26 @@ function Form() {
 }
 
 function PackingList() {
-  return <div className="list"> LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item}></Item>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span>
+        {item.quantity} {item.description}
+      </span>
+      <button> ❌ </button>
+    </li>
+  );
 }
 
 function Stats() {

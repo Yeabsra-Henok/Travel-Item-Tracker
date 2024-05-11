2831f73caddcccadb487b0765ae6a7d91066172c
Yeabsra Henok <yeabsrahenok0909@gmail.com>
2024-05-11 21:32:24 +0300

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import { getValue } from "@testing-library/user-event/dist/utils";
-
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },

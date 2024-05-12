9ce36543bdeb3530a9d8aa2a4ba34d07fdacf3e5
Yeabsra Henok <yeabsrahenok0909@gmail.com>
2024-05-12 18:59:55 +0300

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
@@ -21,6 +23,7 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDescription] = useState("");
   function handleSubmit(e) {
     e.preventDefault();
     console.log(e);
@@ -40,7 +43,7 @@ function Form() {
         ))}
       </select>
 
-      <input type="text" placeholder="Item..." />
+      <input type="text" placeholder="Item..." value={description} onChange={(e)=} />
       <button> Add </button>
     </form>
   );

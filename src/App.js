733cd5fc17f29e7dd1c89aa47706cddac2d77244
Yeabsra Henok <yeabsrahenok0909@gmail.com>
2024-05-13 17:11:35 +0300

    initialize default state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { useState } from "react";
 
-// const initialItems = [
-//   { id: 1, description: "Passports", quantity: 2, packed: false },
-//   { id: 2, description: "Socks", quantity: 12, packed: false },
-//   { id: 3, description: "charger", quantity: 1, packed: true },
-// ];
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "charger", quantity: 1, packed: true },
+];
 
 export default function App() {
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState(initialItems);
 
   function handleAddItems(item) {
     setItems((items) => [...items, item]);

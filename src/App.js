3bb8f67702a62485d02d0d0c0774073f8ba398b9
Yeabsra Henok <yeabsrahenok0909@gmail.com>
2024-05-19 13:16:08 +0300

    fix defautlt value state conflict

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 3, description: "charger", quantity: 1, packed: true },
+  { id: 3, description: "charger", quantity: 1, packed: false },
 ];
 
 export default function App() {
@@ -34,7 +34,7 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -85,6 +85,7 @@ function Form({ onAddItems }) {
 }
 
 function PackingList({ items, onDeleteItem, onToggleItem }) {
+  const [sortBy, setSortBy] = useState("packed");
   return (
     <div className="list">
       <ul>
@@ -97,6 +98,13 @@ function PackingList({ items, onDeleteItem, onToggleItem }) {
           ></Item>
         ))}
       </ul>
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input"> Sort by input order</option>
+          <option value="description>"> Sort By Description</option>
+          <option value="packed"> Sort By packed Status</option>
+        </select>
+      </div>
     </div>
   );
 }
@@ -118,10 +126,23 @@ function Item({ item, onDeleteItem, onToggleItem }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list 🚀</em>
+      </p>
+    );
+  const numItems = items.length;
+  const numpacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numpacked / numItems) * 100);
   return (
     <footer className="stats">
-      <em>You have X items on your list, and you already packed X</em>
+      <em>
+        {percentage === 100
+          ? "You got everything! Ready to go ✈️"
+          : `💼 You have ${numItems} items on your list and you already packed ${numpacked} (${percentage} %)`}
+      </em>
     </footer>
   );
 }
